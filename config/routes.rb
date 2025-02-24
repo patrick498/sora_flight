5c8b6ee1b597331bb06238b59b8fb821d237af84
Andre Yuki Ide <andreyukiide@gmail.com>
2025-02-24 20:10:39 +0900

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: "pages#home" #doing this at some point
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  resources :games, only: [:show, :update]
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  get '/play', to: 'games#play'
+
 end
