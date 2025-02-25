7c0b434effefaa2117b0a3a908e66a2ad6fdcd90
Andre Yuki Ide <andreyukiide@gmail.com>
2025-02-25 12:17:33 +0900

    seeds for a flight

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'open-uri'
+
 puts "Cleaning the db...\n"
 puts "Destroying Users\n"
 User.destroy_all
@@ -31,5 +33,51 @@ puts "############"
 # Add Aircrafts
 
 # Add Flights
+# meguro_lat = 35.686963
+# meguro_lon = 139.749462
+# url = "https://opendata.adsb.fi/api/v2/lat/#{meguro_lat}/lon/#{meguro_lon}/dist/25"
+
+puts "Creating a flight"
+
+departure_airport = Aiport.where(iata: "NRT")
+arrival_airport = Airport.where(iata: "ICN")
+airline = Airline.where(icao: "JJA")
+aircraft = Aircraft.where(model_short: "B738")
+
+Flight.create!(
+  flight_number: "JJA1102",
+  arrival_airport: arrival_airport,
+  departure_airport: departure_airport,
+  airline: airline,
+  aircraft: aircraft,
+  departure_date: "2025-02-25T11:35:00+00:00",
+  arrival_date: "2025-02-25T14:30:00+00:00",
+  latitude: 35.5762,
+  longitude: 140.511,
+  altitude: 1965,
+  heading: 149,
+  speed: 516
+)
+
+departure_airport = Aiport.where(iata: "PEK")
+arrival_airport = Airport.where(iata: "HND")
+airline = Airline.where(icao: "ANA")
+aircraft = Aircraft.where(model_short: "B763")
+
+Flight.create!(
+  flight_number: "ANA964",
+  arrival_airport: arrival_airport,
+  departure_airport: departure_airport,
+  airline: airline,
+  aircraft: aircraft,
+  departure_date: "2025-02-25T08:25:00+00:00",
+  arrival_date: "2025-02-25T12:40:00+00:00",
+  latitude: 35.1318,
+  longitude: 139.958,
+  altitude: 2964,
+  heading: 41,
+  speed: 489
+)
+
 
 # Add Games
