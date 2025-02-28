6181b3fd2f3caacf1f1747a9f1cf0c939cd5e018
Andre Yuki Ide <andreyukiide@gmail.com>
2025-02-28 17:01:22 +0900

    sdfd

---
+++ public/service-worker.js
@@ -0,0 +1,19 @@
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("pwa-cache-v1").then((cache) => {
+      return cache.addAll([
+        "/",
+        "/assets/application.css",
+        "/assets/application.js"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
