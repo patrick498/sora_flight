ccd6114aa3f9de67c1a7514dc9c121430c9bb0bc
Andre Yuki Ide <andreyukiide@gmail.com>
2025-02-28 16:04:46 +0900

    tring to do PWA

--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,8 @@ Rails.application.routes.draw do
   get '/setup', to: 'games#setup', as: :setup
   get '/load_game', to: 'games#load_game', as: :game_load
 
+  # Routes for PWA
+  get "/service-worker.js", "service_worker#service_worker", as: :service_worker
+  get "/manifest.json", "service_worker#manifest", as: :manifest
+
 end
