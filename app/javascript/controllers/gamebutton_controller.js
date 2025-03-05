3d37aea14054c7501691a71fc71f06abbad51429
Axel Bailleres <axxelb14@gmail.com>
2025-03-05 02:59:56 -0600

    changes on the quiz form

--- app/javascript/controllers/gamebutton_controller.js
+++ app/javascript/controllers/gamebutton_controller.js
@@ -7,11 +7,13 @@ export default class extends Controller {
     secondAnswer: Number,
     thirdAnswer: Number,
   }
-  static targets = ["button", "quiz", "dashedSquare", "firstQuestion", "secondQuestion","thirdQuestion"]
+  static targets = ["button", "quiz", "dashedSquare", "firstQuestion", "secondQuestion", "thirdQuestion", "answer"]
 
   connect() {
     console.log("connected")
     this.questionNumber = 1
+    console.log(this.element.children);
+
   }
 
   showQuiz() {
@@ -22,54 +24,72 @@ export default class extends Controller {
     this.dashedSquareTarget.classList.add("d-none");
   }
 
-    next(){
-      console.log("Next");
-      //increment the a number
-      this.questionNumber += 1;
-      //hide all question html elements
-      const questions = [this.firstQuestionTarget,this.secondQuestionTarget,this.thirdQuestionTarget];
-      questions.forEach((question) => {
-        question.classList.add("d-none")
+  next() {
+    console.log("Next");
+    //increment the a number
+    this.questionNumber += 1;
+    //hide all question html elements
+    const questions = [this.firstQuestionTarget, this.secondQuestionTarget, this.thirdQuestionTarget];
+    questions.forEach((question) => {
+      question.classList.add("d-none")
+    })
+    //Show the curent quesstion
+    questions[this.questionNumber - 1].classList.remove("d-none")
+  }
+
+  checkAnswer(e) {
+    const selectedValue = e.currentTarget.value;
+    const selectedAnswer = e.currentTarget.closest('.arrival-airport-item');
+
+    let isCorrect;
+    let correctAnswer;
+
+    if (selectedValue == this.firstAnswerValue) {
+      isCorrect = true;
+      correctAnswer = this.firstAnswerValue;
+    } else if (selectedValue == this.secondAnswerValue) {
+      isCorrect = true;
+      correctAnswer = this.secondAnswerValue;
+    } else if (selectedValue == this.thirdAnswerValue) {
+      isCorrect = true;
+      correctAnswer = this.thirdAnswerValue;
+    } else {
+      isCorrect = false;
+    }
+
+    if (isCorrect) {
+      selectedAnswer.classList.add('correct');
+    } else {
+      selectedAnswer.classList.add('incorrect');
+      this.answerTargets.forEach((answerTarget) => {
+
+        const answerValue = answerTarget.value;
+        console.log([this.firstAnswerValue, this.secondAnswerValue, this.thirdAnswerValue], Number(answerValue));
+        console.log([this.firstAnswerValue, this.secondAnswerValue, this.thirdAnswerValue].includes(Number(answerValue)));
+
+        if ([this.firstAnswerValue, this.secondAnswerValue, this.thirdAnswerValue].includes(Number(answerValue))) {
+          answerTarget.parentElement.classList.add("correct");
+        }
       })
-      //Show the curent quesstion
-      questions[this.questionNumber - 1].classList.remove("d-none")
     }
 
-    checkAnswer(e) {
-      const selectedValue = e.currentTarget.value;
-      const selectedAnswer = e.currentTarget.closest('.arrival-airport-item');
-
-      let isCorrect;
-      let correctAnswer;
-
-      if (selectedValue == this.firstAnswerValue) {
-        isCorrect = true;
-        correctAnswer = this.firstAnswerValue;
-      } else if (selectedValue == this.secondAnswerValue) {
-        isCorrect = true;
-        correctAnswer = this.secondAnswerValue;
-      } else if (selectedValue == this.thirdAnswerValue) {
-        isCorrect = true;
-        correctAnswer = this.thirdAnswerValue;
-      } else {
-        isCorrect = false;
-      }
-
-      if (isCorrect) {
-        selectedAnswer.classList.add('correct');
-      } else {
-        selectedAnswer.classList.add('incorrect');
-      }
-
-      // third question, submit form
-      if (e.currentTarget.name === 'game[airline_guess_id]') {
-        e.currentTarget.form.submit()
-      }
-
-      setTimeout(() => {
-        this.next();
-      }, 1000);
+    // third question, submit form
+    if (e.currentTarget.name === 'game[airline_guess_id]') {
+      e.currentTarget.form.submit()
     }
+
+    setTimeout(() => {
+      this.answerTargets.forEach((answerTarget) => {
+        const answerValue = answerTarget.value;
+        console.log(answerValue);
+
+        if ([this.firstAnswerValue, this.secondAnswerValue, this.thirdAnswerValue].includes(Number(answerValue))) {
+          answerTarget.parentElement.classList.remove("correct");
+        }
+      })
+      this.next();
+    }, 2000);
+  }
 
   captureScreenshot() {
     const scene = document.querySelector('a-scene')
